363303eb0492fb7112edf8f3db57942d2ca6415d
pranayh24 <pranayheda24@gmail.com>
2024-12-04 04:27:48 +0530

    hiugiugui\\\\

--- src/main/java/org/prh/utils/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.prh.utils;
-
-public class Constants {
-    public static final int DEFAULT_PORT = 8188;
-    public static final String SERVER_START_MSG = "Chat Server started on port {}";
-
-}
